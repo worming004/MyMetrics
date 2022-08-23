96595c327d39ed6846595161bbe39b8d36f0eaa1
worming004 <worming@hotmail.be>
2022-08-23 17:54:47 +0200

    initial commit

---
+++ MyMetrics.Api/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prometheus;
+
+namespace MyMetrics.Api.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public Counter GetMetric => Metrics.CreateCounter("my_counter", "my counter", new CounterConfiguration
+        {
+            LabelNames = new string[] {"tenant", "status"}
+        });
+        
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            GetMetric.WithLabels("Home", "Ok").Inc();
+            return Ok("Ok");
+        }
+        
+        [HttpGet("Error")]
+        public async Task<IActionResult> GetError()
+        {
+            GetMetric.WithLabels("Home", "Error").Inc();
+            return BadRequest("Error");
+        }
+    
+    }
+}
